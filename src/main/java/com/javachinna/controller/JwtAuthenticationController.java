4a92a891a3b55b79030dc5b8d82ba2652e868810
Chinna <inbox4chinna@gmail.com>
2021-09-29 13:39:31 +0530

    code clean-up

--- src/main/java/com/javachinna/controller/JwtAuthenticationController.java
+++ src/main/java/com/javachinna/controller/JwtAuthenticationController.java
@@ -3,8 +3,6 @@ package com.javachinna.controller;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -30,19 +28,9 @@ public class JwtAuthenticationController {
 
 	@PostMapping("/authenticate")
 	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
-		Authentication authentication = authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
 		final UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 		final String token = jwtTokenUtil.generateToken(userDetails);
 		return ResponseEntity.ok(new JwtResponse(token));
-	}
-
-	private Authentication authenticate(String username, String password) throws Exception {
-		try {
-			return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-		} catch (DisabledException e) {
-			throw new Exception("USER_DISABLED", e);
-		} catch (BadCredentialsException e) {
-			throw new Exception("INVALID_CREDENTIALS", e);
-		}
 	}
 }
